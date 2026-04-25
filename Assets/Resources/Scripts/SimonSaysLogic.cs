3767f6e4e5c282c3a19839eee93517435cb73ade
Lewark <llewark@colostate.edu>
2026-04-24 18:55:14 -0600

    button prefab done maybe?

--- Assets/Resources/Scripts/SimonSaysLogic.cs
+++ Assets/Resources/Scripts/SimonSaysLogic.cs
@@ -6,6 +6,7 @@ public class SimonSaysLogic : MonoBehaviour
     public static List<int> gameRounds = new List<int>();
     public static bool allowedInput = false;
     public static int currentIndex = 0;
+    public static string[] types = {"redButton", "greenButton", "blueButton", "yellowButton"};
 
     public static void Start(){
         Random.InitState(42);
@@ -21,6 +22,10 @@ public class SimonSaysLogic : MonoBehaviour
         if (button == gameRounds[currentIndex]) {
             var test = 2;
         }
+    }
+
+    public static void lightButton(int index){
+        
     }
 
     public static void lightUp(){
